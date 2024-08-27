cd3f176bae1b7df6d2f3e4fdc405c1e7d39b7cf7
Moonwalkert3ch <6620@holbertonstudents.com>
2024-08-27 12:46:11 -0500

    added Footer component successfully with links

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Header } from "./components/Header";
 import { Section } from "./components/Section";
 import { HelpfulResource } from "./components/HelpfulResource";
 import { AboutMe } from "./components/AboutMe";
+import { Footer } from "./components/Footer";
 
 
 function App() {
@@ -49,6 +50,7 @@ function App() {
       />
     </Section>
     <AboutMe />
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import links from "../assets/links.json";
+
+
+export function Footer() {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <ul>
+                {links.map((item, index) => (
+                    <li key={index}>
+                        <a href={item.link} target="_blank" rel="noopener noreferrer">
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <p>&copy; {currentYear} Atlas School</p>
+        </footer>
+    );
+}
